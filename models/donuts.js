2c71b8e776b6b73ea2abe752cbd056fae2693657
Timothy Cross <timothycross1994@gmail.com>
2019-08-27 10:10:27 -0700

    Updated Schema

--- models/donuts.js
+++ models/donuts.js
@@ -1,9 +1,22 @@
 var mongoose = require("mongoose")
 
+
+
+var donutSchema = new mongoose.Schema({
+  Name: String,
+  Date: Date,
+  comment: String, 
+  address: String, 
+  image: String 
+}, {
+  timestamps: true
+});
+
 var eaterSchema = new mongoose.Schema({
     name: String,
     email: String,
     avatar: String,
+    donut: [donutSchema],
     googleId: String
   }, {
     timestamps: true
